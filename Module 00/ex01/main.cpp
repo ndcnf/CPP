d04de1cc37dab2c36eb8c373b99e167b4d71a468
Nadia Chennaf <nadia.chennaf@student.42lausanne.ch>
2022-12-16 15:03:40 +0100

    00/01 main prompt, phonebook called

--- Module 00/ex01/main.cpp
+++ Module 00/ex01/main.cpp
@@ -6,23 +6,22 @@
 /*   By: nchennaf <nchennaf@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/16 10:00:52 by nchennaf          #+#    #+#             */
-/*   Updated: 2022/12/16 10:46:30 by nchennaf         ###   ########.fr       */
+/*   Updated: 2022/12/16 15:02:15 by nchennaf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include	<iostream>
 #include	"Class.Phonebook.hpp"
-// #include	"Class.Contact.hpp"
+#include	"Class.Contact.hpp"
 
 int	main(void)
 {
-	// Phonebook	book;
-	// Contact		person;
+	Phonebook	book;
 	std::string	prompt;
 
 	std::cout << "Welcome to this awesome phonebook !" << std::endl;
 	std::cout << "Enter ADD, SEARCH or EXIT to proceed" << std::endl;
 	std::cin >> prompt;
-	std::cout << "You entered: " << prompt << std::endl;
+	std::cout << "You entered: [" << prompt << "]" << std::endl;
 	return (0);
 }
