0c2db4aa8279c50c6aa2ced1bbfdd6ccbacd93e1
Nadia Chennaf <nadia.chennaf@student.42lausanne.ch>
2022-12-23 17:23:22 +0100

    01/04 ex04 early stages

---
+++ Module 01/ex04/main.cpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nchennaf <nchennaf@student.42lausanne.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/23 17:14:18 by nchennaf          #+#    #+#             */
+/*   Updated: 2022/12/23 17:21:56 by nchennaf         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "substitute.hpp"
+
+int	main(int argc, char *argv[])
+{
+	if (argc != 4)
+	{
+		std::cout	<< "wrong number of arguments"
+					<< std::endl;
+		return (1);
+	}
+	return (0);
+}
