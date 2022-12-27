078c52a176812fbdc7ad7bee01bb94a4b1032d3a
Nadia Chennaf <nadia.chennaf@student.42lausanne.ch>
2022-12-27 16:15:57 +0100

    01/04 replace OK

--- Module 01/ex04/main.cpp
+++ Module 01/ex04/main.cpp
@@ -6,23 +6,42 @@
 /*   By: nchennaf <nchennaf@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/23 17:14:18 by nchennaf          #+#    #+#             */
-/*   Updated: 2022/12/27 13:47:51 by nchennaf         ###   ########.fr       */
+/*   Updated: 2022/12/27 15:59:35 by nchennaf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "substitute.hpp"
+#include <iostream>
+#include <string>
+#include <fstream>
 
+std::string	getFileContent(std::string file)
+{
+	std::ifstream	ifs(file);
+	std::string		content;
 
-// std::string	getFileContent(std::string file)
-// {
-
-// }
+	if (ifs.is_open())
+	{
+		char c = ifs.get();
+		while (ifs.good())
+		{
+			content += c;
+			c = ifs.get();
+		}
+	}
+	else
+	{
+		std::cout	<< "Error: Opening file impossible"
+					<< std::endl;
+	}
+	ifs.close();
+	return (content);
+}
 
 int	main(int argc, char *argv[])
 {
 	if (argc != 4)
 	{
-		std::cout	<< "wrong number of arguments"
+		std::cout	<< "Error: Wrong number of arguments"
 					<< std::endl;
 		return (1);
 	}
@@ -31,35 +50,40 @@ int	main(int argc, char *argv[])
 	//TODO verifier si le fichier est lisible (droits de lecture)
 	//TODO Remplacer les OCCURENCES, pas les mots entiers. Voir https://cplusplus.com/reference/string/string/?kw=string (erase, substr, ...)
 
-	std::ifstream	ifs(argv[1]);
-	std::string		word;
-	std::string		content;
-	// std::size_t		pos;
+	std::string		fileContent;
 
-	if (ifs.is_open())
+	fileContent = getFileContent(argv[1]);
+	std::cout << fileContent; //NOTE Uniquement pour tester
+
+	std::string		before = argv[2];
+	std::string		after = argv[3];
+	std::size_t		pos = fileContent.find(before);
+
+	std::cout << "length: " << fileContent.length() << std::endl;
+	std::cout << "pos-find: " << pos << std::endl;
+
+	while (pos!= std::string::npos)
 	{
-		char c = ifs.get();
-		while (ifs.good())
-		{
-			// ifs >> word;
+		fileContent.erase(pos, before.length());
+		fileContent.insert(pos, after);
+		pos = fileContent.find(before, pos+1);
+	}
 
-			// std::cout << c;
-			content += c;
-			c = ifs.get();
+	std::cout << "APRES: " << fileContent; //NOTE Uniquement pour tester
 
-			if (argv[2] == word)
-				word = argv[3];
-			else
-			{
-				int	j = 0;
-				for (int i = 0; i < word.length(); i++)
-				{
-					if (word[i] == argv[2][j])
-						// std::cout << word[i] << "-";
-					j++;
-				}
+			// if (argv[2] == word)
+			// 	word = argv[3];
+			// else
+			// {
+			// 	int	j = 0;
+			// 	for (int i = 0; i < word.length(); i++)
+			// 	{
+			// 		if (word[i] == argv[2][j])
+			// 			// std::cout << word[i] << "-";
+			// 		j++;
+			// 	}
 
-			}
+			// }
 
 			// if (!ifs.eof())
 			// 	content += " ";
@@ -67,27 +91,10 @@ int	main(int argc, char *argv[])
 			// content += word;
 			// pos = word.find(argv[2]);
 			// std::cout << pos << std::endl;
-		}
-		std::cout << content;
-	// 	while (ifs.good())
-	// 	{
-	// 		std::cout << c;
-	// 		c = ifs.get();
-	// 	}
-	}
-	else
-	{
-		std::cout	<< "Error opening file"
-					<< std::endl;
-	}
-	//NOTE Fin de l'exemple
+		// }
 
-	//NOTE peut-etre https://cplusplus.com/reference/istream/istream/read/
 
-	// std::cout	<< before
-	// 			<< std::endl;
 
-	ifs.close();
 
 	//////////////////////////////////////////////////
 	// //SECTION - File name OK
@@ -99,7 +106,6 @@ int	main(int argc, char *argv[])
 	//////////////////////////////////////////////////
 
 	// std::ofstrean	ofs(file);
-
 
 	return (0);
 }
