f6abf926ddda839ea6d2664927bbc35022a77866
Nadia Chennaf <nadia.chennaf@student.42lausanne.ch>
2023-01-25 13:42:15 +0100

    07/01 early stage, wip & ex00 tests made

---
+++ Module 07/ex01/iter.hpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   iter.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nchennaf <nchennaf@student.42lausanne.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/25 12:54:33 by nchennaf          #+#    #+#             */
+/*   Updated: 2023/01/25 12:58:24 by nchennaf         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ITER_HPP
+# define ITER_HPP
+
+# include <iostream>
+
+template<typename A, typename B, typename C>
+void	iter(A *tab, B tabSize, C )
+
+#endif
