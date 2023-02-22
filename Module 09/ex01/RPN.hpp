d2ca0e0ab96f98cfe9aeb2054e61e89f509f600a
Nadia Chennaf <nadia.chennaf@student.42lausanne.ch>
2023-02-22 17:43:01 +0100

    end of day, wednesday

--- Module 09/ex01/RPN.hpp
+++ Module 09/ex01/RPN.hpp
@@ -3,6 +3,7 @@
 
 # include <iostream>
 # include <stack>
+# include <algorithm>
 
 class RPN
 {
--- Module 09/ex01/main.cpp
+++ Module 09/ex01/main.cpp
@@ -2,10 +2,28 @@
 
 int	main(int argc, char *argv[])
 {
-	(void)argc;
-	(void)argv;
+	// (void)argv;
+
+	if (argc != 2)
+		return (1);
+
+	std::string	str(argv[1]);
 
 	std::stack<int>	stack;
+	// std::stack<int>::iterator	it; //ITERATOR A INTEGRER
+
+	for (unsigned int i = 0; i < str.length(); i++)
+	{
+		if (str[i] == ' ')
+			i++;
+		if (str[i] == '*')
+		{
+			std::cout << "hello: " << str[i] << std::endl;
+		}
+		if (str[i] == '+')
+		if (str[i] == '-')
+		if (str[i] == '/')
+	}
 
 	return (0);
 }
